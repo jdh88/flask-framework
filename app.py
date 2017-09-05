2c2ccd691a7479da9935d3067ecd87e2b75db06e
Jonathan Hirokawa <johirokawa@gmail.com>
2017-09-05 00:11:46 -0400

    Basic version up and running!

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask import flash
 from forms import TickerSymbolForm
 
+from stocks import get_stock_data, plot_stocks
+
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -9,12 +11,18 @@ app.config.from_object('config')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = TickerSymbolForm()
+    script = None
+    div = None
     if form.validate_on_submit():
-        flash('Company: %s' % (form.ticker_symbol.data))
-        #flash('The following data was submitted: \n'
-        #      + 'Ticker Symobl: %s' % (form.ticker_symbol.data))
-        return redirect('/')
-    return render_template('index.html', form=form)
+        company = form.ticker_symbol.data
+
+        flash('You entered: %s' % (company))
+
+        sdata = get_stock_data(company, '20170801', '20170831')
+        script, div = plot_stocks(sdata, company)
+
+        #return redirect('/')
+    return render_template('index.html', form=form, script=script, div=div)
 
 if __name__ == '__main__':
   app.run(port=33507)
---
+++ stocks.py
@@ -0,0 +1,76 @@
+"""
+Helper functions that get stock data and plot it, returning html and javascript code to embedd it
+"""
+
+import requests
+import pandas as pd
+from bokeh import plotting as bk
+from bokeh.embed import components
+from bokeh.models import HoverTool
+
+def get_stock_data(company, start_date_inc, stop_date_inc):
+    """
+    Gets the stock data for a given company between two dates
+
+    :company: the company ticker name
+    :param start_date_inc: the start date of interest (inclusive)
+    :param stop_date_inc: the end date of interest (inclusive)
+
+    :return: pandas data table with the stock data
+    """
+
+    api_key = 'Bo9P_cJnmf5EsQPp1Bdp'
+    desired_cols = 'date,close'
+
+#    ticker = 'FB'
+#    start_date_inc = '20170801'
+#    end_date_inc = '20170831'
+
+    # format and send the request
+    payload = {
+        'date.gte': start_date_inc,
+        'date.lte': stop_date_inc,
+        'ticker': company,
+        'qopts.columns': desired_cols,
+        'api_key': api_key
+    }
+    meta_url = r'https://www.quandl.com/api/v3/datatables/WIKI/PRICES'
+    r = requests.get(meta_url, params=payload)
+
+    # convert to a pandas dataframe
+    df = pd.DataFrame(r.json()['datatable']['data'])
+    df.columns = ['date', 'price']
+    df['date'] = pd.to_datetime(df['date'])
+
+    return df
+
+def plot_stocks(df, ticker):
+    """
+    :param df: the pandas data frame with the stock data and datatables
+    :param ticker: the name of the company (used in title)
+
+    :return: script and div elements for a bokeh based plot
+    """
+    title = 'Closing prices for: ' + ticker
+    f = bk.figure(title=title, x_axis_type='datetime')
+    f.xaxis.axis_label = 'Date'
+    f.yaxis.axis_label = 'Closing Price (USD)'
+
+    source = bk.ColumnDataSource(data=dict(
+        x = df['date'],
+        y = df['price']))
+
+    hover = HoverTool(
+        tooltips=[
+            ('Date:', '@x{%F}'),
+            ('Price:', '@y{($ 0.00 a)}'),
+        ],
+        formatters={
+            'x': 'datetime',
+            'y': 'numeral'
+        })
+    f.add_tools(hover)
+
+    f.line('x', 'y', source=source)
+
+    return components(f)
